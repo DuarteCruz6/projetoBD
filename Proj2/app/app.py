b457ea8002ffc96840d125d509f4323168f360aa
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-06-12 12:13:57 +0100

    API done

--- Proj2/app/app.py
+++ Proj2/app/app.py
@@ -71,7 +71,6 @@ def aeroport_index():
                 """,
                 {},
             ).fetchall()
-            log.debug(f"Found {cur.rowcount} rows.")
             
     if not aeroports:
         return jsonify({"message": "No aeroports found.", "status": "error"}), 404
@@ -119,12 +118,18 @@ def show_flights_12hour(partida):
 def show_3_flights_with_tickets(partida, chegada):
     """Shows the next 3 flights between <partida> and <chegada> that still have tickets available to purchase"""
     """flight = (no_serie, hora_partida)"""
-
+    
+    if partida == chegada:
+        return jsonify({"message": "Foi selecionado o mesmo aeroporto na chegada e na partida.", "status": "error"}), 404
+    
+    timeNow = datetime.datetime.now()
     with pool.connection() as conn:
         with conn.cursor() as cur:
             flights = cur.execute(
                 """
-                SELECT voo.no_serie, voo.partida, voo.chegada, voo.hora_partida
+                SELECT voo.id, voo.no_serie, voo.hora_partida,
+                    COALESCE(bilhete.bilhetes_emitidos, 0) AS bilhetes_emitidos,
+                    COALESCE(assento.total_assentos, 0) AS total_assentos
                 FROM voo
                 JOIN aviao ON voo.no_serie = aviao.no_serie 
                 
@@ -142,15 +147,15 @@ def show_3_flights_with_tickets(partida, chegada):
                 
                 WHERE partida = %(partida)s
                 AND chegada = %(chegada)s
+                AND hora_partida > %(timeNow)s 
                 
                 AND COALESCE(bilhete.bilhetes_emitidos, 0) < COALESCE(assento.total_assentos, 0)
                 
                 ORDER BY voo.hora_partida ASC
                 LIMIT 3;
                 """,
-                {"partida": partida, "chegada": chegada, },
+                {"partida": partida, "chegada": chegada, "timeNow":timeNow },
             ).fetchall()
-            log.debug(f"Found {cur.rowcount} rows.")
 
     if not flights:
         return jsonify({"message": "No flights found with tickets available between those two airports.", "status": "error"}), 404
@@ -234,48 +239,25 @@ def createVenda(cur, nif_cliente):
         """
             INSERT INTO venda (codigo_reserva, nif_cliente, balcao, hora)
             VALUES (%(codigo_reserva)s, %(nif_cliente)s, %(balcao)s, %(hora)s)
-            RETURNING id;
+            RETURNING codigo_reserva;
         """, 
         {
             "codigo_reserva":last_codigo_reserva+1, "nif_cliente":nif_cliente, 
-            "balcao":"---", "hora":datetime.now().time()
+            "balcao":None, "hora":datetime.datetime.now()
         }
     )
     
     return last_codigo_reserva+1
-
-
-def getSeat(cur,no_serie,prim_classe):
-    cur.execute(
-        """
-        SELECT assento.lugar
-        FROM assento
-        WHERE assento.no_serie = %(no_serie)s
-        AND assento.prim_classe = %(prim_classe)s
-        AND NOT EXISTS (
-            SELECT 1
-            FROM bilhete
-            WHERE bilhete.no_serie = assento.no_serie AND bilhete.lugar = assento.lugar
-        )
-        LIMIT 1
-        """,
-        {"no_serie": no_serie, "prim_classe": prim_classe}
-    )
-    row = cur.fetchone()
-    return row[0] if row else None
     
 def createTickets(cur, voo_id, pairs, nif_cliente, tempo_voo):
     #gets the last ticketId for this flight
     cur.execute(
         """
             SELECT id FROM bilhete
-            WHERE voo_id = %(voo_id)s
             ORDER BY id DESC
             LIMIT 1
         """,
-        {
-            "voo_id":voo_id,
-        } 
+        {} 
     )
     row = cur.fetchone()
     if not row:
@@ -299,43 +281,35 @@ def createTickets(cur, voo_id, pairs, nif_cliente, tempo_voo):
     no_serie = row[0]
     
     codigo_reserva = createVenda(cur, nif_cliente)
-    
+    listTicketsIds = []
     for pair in pairs:
         print(pair)
         # pair = (nome_passageiro, classe de bilhete)
         #creates <bilhete>
-        if pair[1]: # This is the line where your error is happening
-            # ... your logic for when the boolean is True ...
-            print(f"DEBUG: {pair[0]} has boolean True.")
-        else:
-            # ... your logic for when the boolean is False ...
-            print(f"DEBUG: {pair[0]} has boolean False.")
         nome_passageiro = pair[0]
         prim_classe = pair[1]
         if prim_classe:
             preco = 60 * tempo_voo
         else:
             preco = 40 * tempo_voo
-        lugar = getSeat(cur, no_serie, prim_classe)
-        
-        if lugar is None:
-            return False
+            
         cur.execute(
             """
-                INSERT INTO bilhete (id, voo_id, codigo_reserva, nome_passageiro, preco, prim_classe, lugar, no_serie)
+                INSERT INTO bilhete (id, voo_id, codigo_reserva, nome_passegeiro, preco, prim_classe, lugar, no_serie)
                 VALUES (
-                    %(id)s,%(voo_id)s,%(codigo_reserva)s,%(nome_passageiro)s,%(preco)s,
+                    %(id)s,%(voo_id)s,%(codigo_reserva)s,%(nome_passegeiro)s,%(preco)s,
                         %(prim_classe)s,%(lugar)s,%(no_serie)s
                 )
             """,
             {
                 "id":last_bilhete_id+offSet, "voo_id":voo_id, "codigo_reserva":codigo_reserva, 
-                "nome_passageiro":nome_passageiro, "preco":preco, "prim_classe":prim_classe,
-                "lugar":lugar,"no_serie":no_serie,
+                "nome_passegeiro":nome_passageiro, "preco":preco, "prim_classe":prim_classe,
+                "lugar": None,"no_serie":no_serie,
             }
         )
+        listTicketsIds.append(last_bilhete_id+offSet)
         offSet+=1
-    return True
+    return listTicketsIds
         
         
 def calculateTempoVoo(cur, voo_id):
@@ -353,15 +327,14 @@ def calculateTempoVoo(cur, voo_id):
     row = cur.fetchone()
     if row:
         partida, chegada = row
-        today = datetime.today()
-        partida_dt = datetime.combine(today, partida)
-        chegada_dt = datetime.combine(today, chegada)
-        tempo_voo = (chegada_dt - partida_dt).seconds / 3600  # em horas
+        #today = datetime.datetime.today()
+        #partida_dt = datetime.datetime.combine(today, partida)
+        #chegada_dt = datetime.datetime.combine(today, chegada)
+        #tempo_voo = (chegada_dt - partida_dt).seconds / 3600  # em horas
+        tempo_voo = (chegada - partida).total_seconds() / 3600
         return tempo_voo
     else:
         return 0
-
-
 
 def getFlight(cur, voo_id):
     cur.execute(
@@ -387,6 +360,15 @@ def buyTickets(voo_id):
     """Receives as arguments nif_cliente, and a list of pairs (nome_passageiro, classe de bilhete)"""
 
     nif_cliente = request.args.get("nif_cliente")
+    
+    try:
+        int(nif_cliente)
+    except:
+        return jsonify({"message": "Nif inválido."}), 400
+    
+    if len(nif_cliente)!=9:
+        return jsonify({"message": "Nif inválido."}), 400
+    
     pairs_jsonList = request.args.get("pairs")
     
     if not nif_cliente or not pairs_jsonList:
@@ -396,18 +378,28 @@ def buyTickets(voo_id):
         pairs = json.loads(pairs_jsonList)
     except json.JSONDecodeError:
         return jsonify({"message": "Formato inválido para pairs", "status": "error"}), 400
+    
+    for pair in pairs:
+        if not len(pair)==2:
+            return jsonify({"message": "Dados inválidos."}), 400
+        
+        if not isinstance(pair[0],str):
+            return jsonify({"message": "Dados inválidos."}), 400
+        
+        if not isinstance(pair[1],bool):
+            return jsonify({"message": "Dados inválidos."}), 400
 
     conn = None
     try:
         with pool.connection() as conn:
+            conn.autocommit = False #desliga commits automaticos para caso haja um erro a meio
             with conn.cursor() as cur:
                 
                 #get flight
                 flight = getFlight(cur,voo_id)
                 if not flight:
                     return jsonify({"message": "Voo não encontrado."}), 400
-                
-                
+                 
                 if not verifyTicketsAvailability(cur,voo_id,pairs):
                     #there are no suficient tickets
                     return jsonify({"message": "Não há lugares suficientes."}), 400
@@ -415,8 +407,9 @@ def buyTickets(voo_id):
                 tempo_voo = calculateTempoVoo(cur, voo_id)
                 if tempo_voo == 0:
                     return jsonify({"message": "Erro a calcular tempo de voo."}), 400
-                    
-                if not createTickets(cur, voo_id, pairs, nif_cliente, tempo_voo):
+                
+                listTicketsId = createTickets(cur, voo_id, pairs, nif_cliente, tempo_voo)
+                if not listTicketsId:
                     return jsonify({"message": "Erro a criar bilhetes."}), 400
                 
             conn.commit()
@@ -429,11 +422,32 @@ def buyTickets(voo_id):
                 pass
         return jsonify({"message": f"Erro interno: {str(e)}"}), 500
 
-    return jsonify({"message": "Bilhetes comprados com sucesso."}), 200
+    return jsonify({"message": f"Bilhetes comprados com sucesso com Ids: {listTicketsId}"}), 200
 
+def getSeat(cur,voo_id,no_serie,prim_classe):
+    cur.execute(
+        """
+        SELECT assento.lugar
+        FROM assento
+        WHERE assento.no_serie = %(no_serie)s
+        AND assento.prim_classe = %(prim_classe)s
+        AND NOT EXISTS (
+            SELECT 1
+            FROM bilhete
+            WHERE bilhete.no_serie = assento.no_serie 
+            AND bilhete.lugar = assento.lugar
+            AND bilhete.voo_id = %(voo_id)s
+        )
+        LIMIT 1
+        FOR UPDATE
+        """,
+        {"no_serie": no_serie, "prim_classe": prim_classe, "voo_id": voo_id}
+    )
+    row = cur.fetchone()
+    return row[0] if row else None
 
 @app.route(
-    "/checkin/<bilhete_id>/",methods=("POST",),
+    "/checkin/<int:bilhete_id>/",methods=("POST",),
 )
 def do_checkIn_ticket(bilhete_id):
     """Does a checkIn of a ticket, giving him the seat from the correspondent class"""
@@ -442,7 +456,7 @@ def do_checkIn_ticket(bilhete_id):
         with conn.cursor() as cur:
             cur.execute(
                 """
-                    SELECT no_serie, prim_classe, lugar FROM bilhete
+                    SELECT voo_id, no_serie, prim_classe, lugar FROM bilhete
                     WHERE id = %(bilhete_id)s
                     LIMIT 1
                 """,
@@ -454,11 +468,11 @@ def do_checkIn_ticket(bilhete_id):
             if not row:
                 return jsonify({"message": "Bilhete não encontrado."}), 400 
             
-            no_serie, prim_classe, lugar_atual = row
+            voo_id, no_serie, prim_classe, lugar_atual = row
             if lugar_atual is not None:
                 return jsonify({"message": "Check-in já efetuado para este bilhete."}), 400
             
-            lugar = getSeat(cur, no_serie, prim_classe)
+            lugar = getSeat(cur, voo_id, no_serie, prim_classe)
             
             if lugar is None:
                 return jsonify({"message": "Erro ao atribuir lugar."}), 400 
